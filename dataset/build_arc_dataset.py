2a83401529af9967b8e44a7fa672cafff2b8e081
Michael Samon <38788931+samon11@users.noreply.github.com>
2025-10-21 19:26:35 -0500

    Update dataset directories and output paths for ARC-2

--- dataset/build_arc_dataset.py
+++ dataset/build_arc_dataset.py
@@ -18,12 +18,12 @@ cli = ArgParser()
 
 class DataProcessConfig(BaseModel):
     # ARC-1
-    dataset_dirs: List[str] = ["dataset/raw-data/ARC-AGI/data", "dataset/raw-data/ConceptARC/corpus"]
-    output_dir: str = "data/arc-aug-1000"
+    # dataset_dirs: List[str] = ["dataset/raw-data/ARC-AGI/data", "dataset/raw-data/ConceptARC/corpus"]
+    # output_dir: str = "data/arc-aug-1000"
     
     # ARC-2
-    # dataset_dirs: List[str] = ["dataset/raw-data/ARC-AGI-2/data"]
-    # output_dir: str = "data/arc-2-aug-1000"
+    dataset_dirs: List[str] = ["dataset/raw-data/ARC-AGI-2/data"]
+    output_dir: str = "data/arc-2-aug-1000"
 
     seed: int = 42
     num_aug: int = 1000
